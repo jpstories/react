7fa4e7538f2ffa4f13ce3b26a23f72f9febbb84c
bool_one <41709736+jpstories@users.noreply.github.com>
2020-02-24 23:14:54 +1000

    Add files via upload

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Cart from '../Cart/Cart';
-import Footer from '../Footer/Footer';
+// import Footer from '../Footer/Footer';
 import './App.css';
 
 function App() {
@@ -17,9 +17,6 @@ function App() {
 
       {/* Cart */}
       <Cart />
-      
-      {/* Footer */}
-      <Footer />
 
     </div>
   );
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import arbuz from './img/arbuz.jpg';
+import dynia from './img/dynia.jpg';
+import kapusta from './img/kapusta.jpg';
 import './Main.css';
 
 function App() {
@@ -9,12 +11,31 @@ function App() {
 
                 <div className="goods">
                     <span className="goods__title">Арбуз "Гавайский"</span>
-                    <img src={arbuz} />
-                    <span className="goods__price">500 руб.</span>
+
+                    <div className="goods__content">
+                        <img src={arbuz} />
+                        <span className="goods__price">500 руб.</span>
+                    </div>
                 </div>
 
-                <div className="goods"></div>
-                <div className="goods"></div>
+                <div className="goods dynia">
+                    <span className="goods__title">Дыня "Окинава"</span>
+
+                    <div className="goods__content">
+                        <img src={dynia} />
+                        <span className="goods__price">1800 руб.</span>
+                    </div>
+                </div>
+
+                <div className="goods kapusta">
+                    <span className="goods__title">Капуста "Цветная"</span>
+
+                    <div className="goods__content">
+                        <img src={kapusta} />
+                        <span className="goods__price">180 руб.</span>
+                    </div>
+                </div>
+
                 <div className="goods"></div>
                 <div className="goods"></div>
                 <div className="goods"></div>
